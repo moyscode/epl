adc5482b9a512e72f735ecdec8d14dd8a8d96200
moyscode <moyscode@gmail.com>
2022-06-08 11:10:44 +0530

    prodConfig object created with ssl

--- db.js
+++ db.js
@@ -3,7 +3,10 @@ dotenv.config({ silent: true });
 
 const devConfig = `${process.env.PG_USER}://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
-const prodConfig = process.env.DATABASE_URL;
+const prodConfig = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+};
 
 export const dbConnection =
-  process.env.NODE_ENV === "production" ? prodConfig + "?ssl=true" : devConfig;
+  process.env.NODE_ENV === "production" ? prodConfig : devConfig;
